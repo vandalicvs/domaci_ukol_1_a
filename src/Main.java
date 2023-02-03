fd38fbe228a92af581ab1a5ceb2490af71df8386
Vandalicus <jamiem@seznam.cz>
2023-02-03 18:08:54 +0100

    test

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        String krestniJmeno;
+        String prijmeni;
+        int pocetSmluv;
+        double sumaProdeje;
+        String mesto;
+        String spz;
+        double spotreba;
+        String ipAdresa;
+
+        double prumerneMnozstviProdeje = sumaProdeje/pocetSmluv;
+
+        System.out.println(prumerneMnozstviProdeje);
+
+    }
+}
